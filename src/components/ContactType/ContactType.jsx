e57f72550c648d5cb48b828e3aab7149f5a77517
Alex <112724167+klimbass@users.noreply.github.com>
2024-08-26 23:55:07 +0300

    set-contactype-component

--- src/components/ContactType/ContactType.jsx
+++ src/components/ContactType/ContactType.jsx
@@ -3,13 +3,20 @@ import css from "./ContactType.module.css";
 
 
 export default function ContactType({contactType}){
-switch(contactType){
-    case 'work':
-        return <div className={`${css.contactType} ${css.workGreen}`}>work</div>;
-    case 'home':
-        return <div className={`${css.contactType} ${css.homeBlue}`}>home</div>;
-    default :
-        return <div className={`${css.contactType} ${css.personalRed}`}>personal</div>
+// switch(contactType){
+//     case 'work':
+//         return <div className={`${css.contactType} ${css.workGreen}`}>work</div>;
+//     case 'home':
+//         return <div className={`${css.contactType} ${css.homeBlue}`}>home</div>;
+//     default :
+//         return <div className={`${css.contactType} ${css.personalRed}`}>personal</div>
     
+// }
+let backColor = 'coral'
+if(contactType !== 'personal'){
+    contactType === 'work'? backColor = 'lightseagreen': backColor = 'cornflowerblue';
 }
+return(
+    <div className={css.contactType} style={{backgroundColor:backColor}}>{contactType}</div>
+);
 }
